33338832ebf814fb3d4ac248552b169c82479ebc
naufalmahing <naufalmahing@gmail.com>
2024-09-05 19:40:27 +0700

    remove log output for choreo

--- main.py
+++ main.py
@@ -47,17 +47,17 @@ def add():
     f = open('/tmp/demo.txt', 'r')
     lines = f.readlines()
     s = '\n'.join(lines)
-    app.logger.debug('add is done no?!')
+    # app.logger.debug('add is done no?!')
     return {'code': 200, 'msg': 'berhasil tambah', 'data': s}
 
-import logging
-import sys
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# import logging
+# import sys
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setLevel(logging.DEBUG)
-flask_cors.rootlogger.removeHandler(flask_cors.rootlogger.handlers[0])
-flask_cors.rootlogger.addHandler(handler)
+# handler = logging.StreamHandler(stream=sys.stdout)
+# handler.setLevel(logging.DEBUG)
+# flask_cors.rootlogger.removeHandler(flask_cors.rootlogger.handlers[0])
+# flask_cors.rootlogger.addHandler(handler)
 
 if __name__ == '__main__':
     app.run(debug=True)
