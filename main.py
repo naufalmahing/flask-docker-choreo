bb588b8d31ad2e6704d38f8f335f0161d72fe0cf
naufalmahing <naufalmahing@gmail.com>
2024-09-07 06:47:06 +0700

    use jwt token

--- main.py
+++ main.py
@@ -6,11 +6,53 @@ from flask import Flask, jsonify, session, request
 from flask_cors import CORS
 import flask_cors
 
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager, set_access_cookies, unset_jwt_cookies
+
+
 app = Flask(__name__)
+
+# Here you can globally configure all the ways you want to allow JWTs to
+# be sent to your web application. By default, this will be only headers.
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+
+# If true this will only allow the cookies that contain your JWTs to be sent
+# over https. In production, this should always be set to True
+app.config["JWT_COOKIE_SECURE"] = False
+
+# Change this in your code!
+app.config["JWT_SECRET_KEY"] = "super-secret"
+
+jwt = JWTManager(app)
 
 app.config['SECRET_KEY'] = 'slkdfjsSDFSDFSDKFLLNNU'
 CORS(app, supports_credentials=True)
 # CORS(app, supports_credentials=True, origins=['http://localhost:3001'])
+
+@app.route("/login_without_cookies", methods=["POST"])
+def login_without_cookies():
+    access_token = create_access_token(identity="example_user")
+    return jsonify(access_token=access_token)
+
+
+@app.route("/login_with_cookies", methods=["POST"])
+def login_with_cookies():
+    response = jsonify({"msg": "login successful"})
+    access_token = create_access_token(identity="example_user")
+    set_access_cookies(response, access_token)
+    return response
+
+
+@app.route("/logout_with_cookies", methods=["POST"])
+def logout_with_cookies():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+
+@app.route("/protected", methods=["GET", "POST"])
+@jwt_required()
+def protected():
+    return jsonify(foo="bar")
 
 @app.route('/set-session/<username>')
 def set_user(username):
